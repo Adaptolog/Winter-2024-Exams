230d05ee856c90ac98ded471ea0f24d425655f89
Adaptolog <143221626+Adaptolog@users.noreply.github.com>
2024-01-14 15:49:06 +0200

    Update shuffle.js
    Instead of using the spread operator to get the array from the input arguments, I can directly destructure the array parameter List to get the arr variable.

--- Tasks/shuffle.js
+++ Tasks/shuffle.js
@@ -1,6 +1,6 @@
 // Shuffle an array
 
-//Step 1: The function name Shuffle has been changed to shuffle.
+//Step 2: Instead of using the spread operator to get the array from the input arguments, I can directly destructure the array parameter List to get the arr variable.
 
 const shuffle = (arr) => {
   arr.sort(() => Math.random() - 0.5);
