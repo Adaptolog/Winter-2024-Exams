df49976b15a9c83e479bb71128fef133f1bd4917
Adaptolog <143221626+Adaptolog@users.noreply.github.com>
2024-01-14 15:44:05 +0200

    Update split.js
    I added the const keyword to declare the begin and end variables.

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,12 +1,12 @@
 // Split an array into two parts
 
-//Step 1: I changed the function name from split_Array to splitArray.
+//Step 2: I added the const keyword to declare the begin and end variables.
 
 let splitArray = (index, array) => {
-  begin = array.slice(0, index);
+  const begin = array.slice(0, index);
   const len = array.length;
-  array = array.slice(index, len);
-  return [begin, array];
+  const end = array.slice(index, len);
+  return [begin, end];
 };
 
 module.exports = splitArray;
