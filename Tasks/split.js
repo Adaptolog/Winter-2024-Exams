778e5a498fd218a76c21414b201858e5ef254a35
Adaptolog <143221626+Adaptolog@users.noreply.github.com>
2024-01-14 15:46:09 +0200

    Update split.js
    I return the begin and end arrays as an array [begin, end] to improve the clarity of the returned values.

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,6 +1,6 @@
 // Split an array into two parts
 
-//Step 3: Instead of using the len variable, I slice the array from the index to the end using array.slice(index).
+//Step 4: I return the begin and end arrays as an array [begin, end] to improve the clarity of the returned values.
 
 let splitArray = (index, array) => {
   const begin = array.slice(0, index);
