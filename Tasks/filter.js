11bd3785b92502534e512dd5623d8b5c29315024
Adaptolog <143221626+Adaptolog@users.noreply.github.com>
2024-01-14 16:08:34 +0200

    Update filter.js
    Returned the filteredArray as the result of the function.

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,7 +1,6 @@
 // Filter array by type name
 
-//Step 2 Used the filter method on the array to create a new array (filteredArray) that contains only the elements of the specified type.
-//The filter method takes a callback function that checks the type of each element using the typeof operator.
+//Step 3: Returned the filteredArray as the result of the function.
 
 const filterArrayByType = (array, typeName) => {
   const filteredArray = array.filter((element) => typeof element === typeName);
