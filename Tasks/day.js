f1e2fe006e8601057ca98e724275a1a2f147aa4f
Adaptolog <143221626+Adaptolog@users.noreply.github.com>
2024-01-14 16:09:29 +0200

    Update day.js
    Renamed the function _parse_day_ to parseDay, Changed variable name D to daysOfWeek.

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,17 @@
 // Get day number
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+// Step 1: Renamed the function _parse_day_ to parseDay, Changed variable name D to daysOfWeek.
 
-_parse_day_ = (s) => {
+const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+const parseDay = (daystring) => {
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < daysOfWeek.length; i++) {
+    if (daystring.startsWith(daysOfWeek[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
