c9de158ea3c8c9ac8c3dc1e1ae41b97a81b6d5fa
Adaptolog <143221626+Adaptolog@users.noreply.github.com>
2024-01-14 17:03:10 +0200

    Update password.js
    Used const instead of let for the max variable, as it does not need to be reassigned.

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,18 +1,19 @@
 // Generate random password
 
-// Step 3: Changed the variable name Index to index to follow camel case naming convention.
+// Step 4: Used const instead of let for the max variable, as it does not need to be reassigned.
 
 const generatePassword = (alphabet, length) => {
-  const MAX = alphabet.length;
+  const max = alphabet.length;
   let password = '';
   for (let i = 0; i < length; i++) {
-    const index = Math.floor(Math.random() * MAX);
+    const index = Math.floor(Math.random() * max);
     password = password + alphabet[index];
   }
   return password;
 };
 
 module.exports = generatePassword;
+
 
 
 
