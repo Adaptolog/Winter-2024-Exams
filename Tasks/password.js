75912be4d4e544f1ceb9981b64c8ee2508027b18
Adaptolog <143221626+Adaptolog@users.noreply.github.com>
2024-01-14 17:02:47 +0200

    Update password.js
    Changed the variable name Index to index to follow camel case naming convention.

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,17 +1,18 @@
 // Generate random password
 
-// Step 2: Changed the variable name key to password to better represent its purpose.
+// Step 3: Changed the variable name Index to index to follow camel case naming convention.
 
 const generatePassword = (alphabet, length) => {
   const MAX = alphabet.length;
   let password = '';
   for (let i = 0; i < length; i++) {
-    Index = Math.floor(Math.random() * MAX);
-    password = password + alphabet[Index];
+    const index = Math.floor(Math.random() * MAX);
+    password = password + alphabet[index];
   }
   return password;
 };
 
 module.exports = generatePassword;
+
 
 
