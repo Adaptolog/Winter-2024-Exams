d811342bc2f485bb8e2019dcba05841b35b7a1ef
Adaptolog <143221626+Adaptolog@users.noreply.github.com>
2024-01-14 15:45:23 +0200

    Update split.js
    Instead of using the len variable, I slice the array from the index to the end using array.slice(index).

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,11 +1,10 @@
 // Split an array into two parts
 
-//Step 2: I added the const keyword to declare the begin and end variables.
+//Step 3: Instead of using the len variable, I slice the array from the index to the end using array.slice(index).
 
 let splitArray = (index, array) => {
   const begin = array.slice(0, index);
-  const len = array.length;
-  const end = array.slice(index, len);
+  const end = array.slice(index);
   return [begin, end];
 };
 
