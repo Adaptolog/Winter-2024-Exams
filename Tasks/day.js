5e6bbb6c56703133477bddffd6cd532ad3b1a7d4
Adaptolog <143221626+Adaptolog@users.noreply.github.com>
2024-01-14 16:10:17 +0200

    Update day.js
    Used const instead of let for the loop variable i, as it is not modified within the loop.

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,12 +1,11 @@
 // Get day number
 
-// Step 2: Used const instead of let for the daysOfWeek array, as the array is not modified and its reference remains constant throughout the code.
+// Step 3: Used const instead of let for the loop variable i, as it is not modified within the loop.
 
 const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (daystring) => {
-  let i;
-  for (i = 0; i < daysOfWeek.length; i++) {
+  for (let i = 0; i < daysOfWeek.length; i++) {
     if (daystring.startsWith(daysOfWeek[i].toLowerCase())) {
       return i + 1;
     }
