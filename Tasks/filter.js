46471e0bcebe6075fb99f7439c6eaee51526eb48
Adaptolog <143221626+Adaptolog@users.noreply.github.com>
2024-01-14 16:07:58 +0200

    Update filter.js
    Step 2 Used the filter method on the array to create a new array (filteredArray) that contains only the elements of the specified type.

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,17 +1,11 @@
 // Filter array by type name
 
-//Step 1: Renaming the function and parameters to comply with the Camel Case naming convention and to provide a more descriptive name.
+//Step 2 Used the filter method on the array to create a new array (filteredArray) that contains only the elements of the specified type.
+//The filter method takes a callback function that checks the type of each element using the typeof operator.
 
 const filterArrayByType = (array, typeName) => {
-  remove = [];
-  for (C of array) {
-    x = array.indexOf(C);
-    if (typeof array[x] !== typeName) {
-      remove.unshift(x);
-    }
-  }
-  for (x of remove) array.splice(x, 1);
-  return array;
+  const filteredArray = array.filter((element) => typeof element === typeName);
+  return filteredArray;
 };
 
 module.exports = filterArrayByType;
